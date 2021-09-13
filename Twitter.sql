794cf865bdccb67f1d48f53f89cd79730d496b4e
beebeatle <ab2009ukr@gmail.com>
2021-09-12 21:06:00 -0400

    Helpful SQL to debug

---
+++ Twitter.sql
@@ -0,0 +1,5 @@
+
+-- Activities 
+SELECT distinct target_user_id FROM `activities` ORDER BY target_user_id DESC;
+
+SELECT distinct target_user_name FROM `activities` ORDER BY target_user_name DESC;
